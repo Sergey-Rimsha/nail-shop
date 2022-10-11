2d041eec3bda81e87067b2faa9475a1969a2cc90
Sergey.rimsha.ru@gmail.com <Sergey.rimsha.ru@gmail.com>
2022-10-11 12:43:08 +0300

    main block

--- src/pages/p0-main/Main.tsx
+++ src/pages/p0-main/Main.tsx
@@ -3,5 +3,19 @@ import { ReactElement } from 'react';
 import s from './Main.module.scss';
 
 export const Main = (): ReactElement => {
-  return <div className={s.main} />;
+  return (
+    <div className={s.main}>
+      <div>
+        <h1>Скидка 30% на все товары бренда Ru Nail</h1>
+        <div>
+          Только до конца этого года, при заказе через интернет- магазин, для Вас
+          действует скидка 30% на все товары бренда Ru Nail!
+        </div>
+        <div>
+          <button type="button">Смотреть каталог</button>
+        </div>
+      </div>
+      <div>img</div>
+    </div>
+  );
 };
