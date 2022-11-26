9fb066f96155eec3009f3b7f0ab854ddb9a04ce2
Sergey.rimsha.ru@gmail.com <Sergey.rimsha.ru@gmail.com>
2022-11-26 22:07:21 +0300

    change style nailNumbers and add data

--- src/pages/p4-nailNumbers/NailNumbers.tsx
+++ src/pages/p4-nailNumbers/NailNumbers.tsx
@@ -1,20 +1,31 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
 import s from './NailNumbers.module.scss';
 
+const state = [
+  { id: 1, title: 10, descriptions: 'Минут максимальное время обработки заказа' },
+  { id: 2, title: 24, descriptions: 'Часа максимальное время доставки по РБ' },
+  { id: 3, title: 50, descriptions: 'Международных брендов в каталоге' },
+  { id: 4, title: 3117, descriptions: 'Цветов в палитре гель-лаков' },
+];
+
 export const NailNumbers = (): ReactElement => {
+  const [items] = useState(state);
+
   return (
     <section className={s.nailNumbers}>
       <div className={s.nailNumbers__title}>
         <h2>Nail Shop в цифрах</h2>
       </div>
       <div className={s.nailNumbers__wrapItems}>
-        <div className={s.item}>
-          <div className={s.item__title}>10</div>
-          <div className={s.item__description}>
-            Минут максимальное время обработки заказа
-          </div>
-        </div>
+        {items.map(el => {
+          return (
+            <div key={el.id} className={s.item}>
+              <div className={s.item__title}>{el.title}</div>
+              <div className={s.item__description}>{el.descriptions}</div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
